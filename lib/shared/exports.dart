bbe6de4488e427935c3cea1a5285ccab1911e737
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-01 00:32:41 +0000

    ✨✏ Worked on onboarding screen

--- lib/shared/exports.dart
+++ lib/shared/exports.dart
@@ -1,3 +1,7 @@
 //Local
 export 'package:green_life/app.dart';
 export 'package:green_life/screens/onboarding_screens.dart/onboarding_screen.dart';
+
+
+//pub.dev
+export 'package:smooth_page_indicator/smooth_page_indicator.dart';
