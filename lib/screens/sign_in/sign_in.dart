c858f51c12daca7c2999c74a4cbd50bd0016a03f
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-02 01:03:51 +0000

    🆕⚙ Created Sign up page

--- lib/screens/sign_in/sign_in.dart
+++ lib/screens/sign_in/sign_in.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:green_life/screens/sign_in/textform.dart';
-import 'package:green_life/widgets/button.dart';
+import '../../shared/exports.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -12,6 +11,9 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   bool obscurePassword = false;
+  final TextEditingController _mobileNumberController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -21,20 +23,12 @@ class _SignInState extends State<SignIn> {
       body: SafeArea(
           child: Padding(
         padding: EdgeInsets.symmetric(
-            horizontal: width * 0.09, vertical: height * 0.03),
+            horizontal: width * 0.02, vertical: height * 0.03),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: EdgeInsets.only(right: width),
-                child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.black,
-                    )),
-              ),
+              backButton(),
               SizedBox(
                 height: height * 0.034,
               ),
@@ -47,60 +41,112 @@ class _SignInState extends State<SignIn> {
                 height: height * 0.061,
               ),
               Form(
-                child: Column(
-                  children: [
-                    textform('Mobile number', TextInputType.number,
-                        const SizedBox.shrink(), obscurePassword),
-                    SizedBox(
-                      height: height * 0.02,
-                    ),
-                    textform(
-                        'Password',
-                        TextInputType.text,
-                        obscurePassword == false
-                            ? IconButton(
-                                icon: const Icon(
-                                  CupertinoIcons.eye,
-                                  color: Color.fromARGB(213, 178, 178, 178),
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    obscurePassword = !obscurePassword;
-                                  });
-                                },
-                              )
-                            : IconButton(
-                                icon: const Icon(CupertinoIcons.eye_slash,
-                                    color: Color.fromARGB(213, 178, 178, 178)),
-                                onPressed: () {
-                                  setState(() {
-                                    obscurePassword = !obscurePassword;
-                                  });
-                                },
-                              ),
-                        obscurePassword),
-                    Row(
-                      children: [
-                        const Expanded(child: SizedBox()),
-                        TextButton(
-                            onPressed: () {},
-                            child: const Text(
-                              'Forgotten password?',
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  color: Color.fromARGB(209, 52, 168, 83)),
-                            ))
-                      ],
-                    ),
-                    SizedBox(
-                      height: height * 0.033,
-                    ),
-                    button(height, width, () {}, 'Log in'),
-                    SizedBox(
-                      height: height * 0.069,
-                    ),
-                    loginOrsignin("Don't have account? ", "Sign up")
-                  ],
+                key: _formKey,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: width * 0.09,
+                  ),
+                  child: Column(
+                    children: [
+                      textform(
+                        'Mobile number',
+                        TextInputType.number,
+                        _mobileNumberController,
+                        (value) {},
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+
+                      //Password field
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: obscurePassword,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Password field cannot be empty';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {},
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: InputDecoration(
+                            hintText: 'Password',
+                            suffixIcon: obscurePassword == true
+                                ? IconButton(
+                                    icon: const Icon(
+                                      CupertinoIcons.eye_slash,
+                                      color: Color.fromARGB(213, 178, 178, 178),
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        obscurePassword = !obscurePassword;
+                                      });
+                                    },
+                                  )
+                                : IconButton(
+                                    icon: const Icon(CupertinoIcons.eye,
+                                        color:
+                                            Color.fromARGB(213, 178, 178, 178)),
+                                    onPressed: () {
+                                      setState(() {
+                                        obscurePassword = !obscurePassword;
+                                      });
+                                    },
+                                  ),
+                            hintStyle: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: Color.fromARGB(218, 178, 178, 178)),
+                            border: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    color: Color.fromARGB(211, 52, 168, 83)),
+                                borderRadius: BorderRadius.circular(20)),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    color: Color.fromARGB(211, 52, 168, 83)),
+                                borderRadius: BorderRadius.circular(20)),
+                            errorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                  color: Color.fromARGB(210, 231, 7, 7)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    color: Color.fromARGB(211, 52, 168, 83)),
+                                borderRadius: BorderRadius.circular(20))),
+                      ),
+                      Row(
+                        children: [
+                          const Expanded(child: SizedBox()),
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Forgotten password?',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: Color.fromARGB(209, 52, 168, 83)),
+                              ))
+                        ],
+                      ),
+                      SizedBox(
+                        height: height * 0.033,
+                      ),
+                      button(height, width, () {
+                        if (_formKey.currentState!.validate()) {
+                          print('Good to go');
+                        }
+                      }, 'Log in'),
+                      SizedBox(
+                        height: height * 0.054,
+                      ),
+                      loginOrsignin(
+                        "Don't have account? ",
+                        "Sign up",
+                        () => Get.to(() => const SignUp()),
+                      )
+                    ],
+                  ),
                 ),
               )
             ],
