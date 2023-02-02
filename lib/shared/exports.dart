f36e5fbe6cc1cb63b59403a8f9f51da4a1a1f59b
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-02 01:04:13 +0000

    🆕⚙ Created Sign up page

--- lib/shared/exports.dart
+++ lib/shared/exports.dart
@@ -1,7 +1,12 @@
 //Local
 export 'package:green_life/app.dart';
 export 'package:green_life/screens/onboarding_screens.dart/onboarding_screen.dart';
-
+export 'package:green_life/screens/sign_in/textform.dart';
+export 'package:green_life/widgets/button.dart';
+export 'package:green_life/screens/sign_up/sign_up.dart';
+export 'package:green_life/screens/sign_in/sign_in.dart';
+export 'package:green_life/widgets/onboarding_widget.dart';
 
 //pub.dev
 export 'package:smooth_page_indicator/smooth_page_indicator.dart';
+export 'package:get/get.dart';
