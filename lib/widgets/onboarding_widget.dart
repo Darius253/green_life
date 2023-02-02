b57525be68801923e1b175cabd59564152d37cb1
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-02 23:45:42 +0000

    🆕✨ Added Splash Screen and made few changes

--- lib/widgets/onboarding_widget.dart
+++ lib/widgets/onboarding_widget.dart
@@ -1,35 +1,72 @@
 import 'package:flutter/material.dart';
 
-Widget onboardingPages(
-    double width, double height, String header, String about, String image) {
-  return Column(children: [
-    Container(
-      height: height * 0.3,
-      decoration: BoxDecoration(
+class OnboardingPages extends StatefulWidget {
+  final double width;
+
+  final double height;
+
+  final String header;
+
+  final String about;
+
+  final String image;
+
+  const OnboardingPages({
+    super.key,
+    required this.about,
+    required this.header,
+    required this.height,
+    required this.image,
+    required this.width,
+  });
+
+  @override
+  State<OnboardingPages> createState() => _OnboardingPagesState();
+}
+
+class _OnboardingPagesState extends State<OnboardingPages> {
+  @override
+  Widget build(BuildContext context) {
+    didChangeDependencies();
+    {
+      precacheImage(AssetImage(widget.image), context);
+    }
+
+    return Column(children: [
+      ClipRRect(
         borderRadius: BorderRadius.circular(36),
-        color: Colors.grey,
+        child: Container(
+          height: widget.height * 0.25,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(36),
+            color: Colors.grey,
+          ),
+          child: Image(
+            image: AssetImage(widget.image),
+          ),
+        ),
       ),
-    ),
-    SizedBox(
-      height: height * 0.02,
-    ),
-    Center(
-      child: Text(
-        header,
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 32,
-            color: Color.fromARGB(221, 81, 81, 81)),
+      SizedBox(
+        height: widget.height * 0.02,
       ),
-    ),
-    Expanded(
-      child: Text(
-        about,
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-            fontWeight: FontWeight.w400, fontSize: 16, color: Colors.black),
+      Center(
+        child: Text(
+          widget.header,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 32,
+              color: Color.fromARGB(221, 81, 81, 81)),
+        ),
       ),
-    ),
-  ]);
+      Expanded(
+        child: Text(
+          widget.about,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              fontWeight: FontWeight.w400, fontSize: 16, color: Colors.black),
+        ),
+      ),
+    ]);
+  }
 }
