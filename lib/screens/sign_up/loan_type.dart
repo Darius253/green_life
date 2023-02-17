2871d38af168dbedb818cee26db9d56e8c2c7a22
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-17 23:56:34 +0000

    🆕✔ Added Pictures

--- lib/screens/sign_up/loan_type.dart
+++ lib/screens/sign_up/loan_type.dart
@@ -28,67 +28,79 @@ class _LoanTypeState extends State<LoanType> {
           padding: EdgeInsets.symmetric(
               horizontal: width * 0.02, vertical: height * 0.03),
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                backButton(),
-                SizedBox(
-                  height: height * 0.05,
-                ),
-                Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: width * 0.05,
-                    ),
-                    child: Center(
-                      child: Column(
-                        children: [
-                          Container(
-                            width: width * 0.8,
-                            height: height * 0.3,
-                            decoration: BoxDecoration(
-                                color: Colors.grey,
-                                borderRadius: BorderRadius.circular(40)),
-                          ),
-                          SizedBox(
-                            height: height * 0.061,
-                          ),
-                          const Text(
-                            'TYPE OF LOAN',
-                            style: TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.w600),
-                          ),
-                          SizedBox(
-                            height: height * 0.01,
-                          ),
-                          const Text(
-                            "Select the type of Loan you want",
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400,
-                                color: Color.fromARGB(213, 178, 178, 178)),
-                          ),
-                          SizedBox(
-                            height: height * 0.035,
-                          ),
-                          button(height, width, () {
-                            Get.to(() => const RegistrationPage());
-                            setState(() {
-                              loanType = "Personal Loan";
-                            });
-                          }, 'Personal Loan'),
-                          SizedBox(
-                            height: height * 0.024,
-                          ),
-                          button(height, width, () {
-                            setState(() {
-                              loanType = "SME's Loan";
-                            });
-                          }, "SME's Loan")
-                        ],
+            child: Stack(children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  backButton(),
+                  SizedBox(
+                    height: height * 0.05,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: width * 0.05,
                       ),
-                    )),
-              ],
-            ),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(36),
+                              child: Container(
+                                height: height * 0.25,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(36),
+                                  color: Colors.grey,
+                                ),
+                                child: const Image(
+                                  image: AssetImage('assets/images/Pic 4.jpg'),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: height * 0.061,
+                            ),
+                            const Text(
+                              'TYPE OF LOAN',
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.w600),
+                            ),
+                            SizedBox(
+                              height: height * 0.01,
+                            ),
+                            const Text(
+                              "Select the type of Loan you want",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color.fromARGB(213, 178, 178, 178)),
+                            ),
+                            SizedBox(
+                              height: height * 0.035,
+                            ),
+                            button(height, width, () {
+                              Get.to(() => const RegistrationPage());
+                              setState(() {
+                                loanType = "Personal Loan";
+                              });
+                            }, 'Personal Loan'),
+                            SizedBox(
+                              height: height * 0.024,
+                            ),
+                            button(height, width, () {
+                              setState(() {
+                                loanType = "SME's Loan";
+                              });
+                            }, "SME's Loan")
+                          ],
+                        ),
+                      )),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: height * 0.7),
+                child: Image.asset('assets/images/Vector.png'),
+              )
+            ]),
           ),
         ),
       ),
