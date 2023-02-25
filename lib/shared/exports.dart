ffd8471d5fac7b3672b17f6c736188d882244ada
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-25 22:00:38 +0000

    ⚙✨ Made changes

--- lib/shared/exports.dart
+++ lib/shared/exports.dart
@@ -14,19 +14,10 @@ export 'package:green_life/screens/sign_up/registration/widgets/textbox.dart';
 export 'package:green_life/screens/sign_up/registration/widgets/loan_summary.dart';
 export 'package:green_life/screens/sign_up/registration/summary.dart';
 export 'package:green_life/screens/sign_up/registration/widgets/picture_upload.dart';
-=======
-export 'package:green_life/screens/image_upload/id_front.dart';
-
 
 //Widgets
-export 'package:green_life/widgets/image_holder.dart';
-export 'package:green_life/widgets/select_document.dart';
-export 'package:green_life/widgets/submit_button.dart';
-export 'package:green_life/widgets/use_camera_button.dart';
-
-
 
 //pub.dev
 export 'package:smooth_page_indicator/smooth_page_indicator.dart';
 export 'package:get/get.dart';
-export 'package:image_picker/image_picker.dart';
+//export 'package:image_picker/image_picker.dart';
