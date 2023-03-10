f414d5e2fd6fe7f1d7606f3b8f36853135a6a561
kelvin <denlinato@gmail.com>
2023-03-10 22:23:17 +0000

    autheticating the user

---
+++ backend/src/middlewares/Auth.ts
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import { BadAuthError } from '@utils/BadAuthError'; 
+import { Payload } from 'app.interface';
+import {NextFunction, Request , Response} from 'express'
+
+declare  global{
+    namespace Express{
+        interface  Request{
+            user:Payload
+        }
+    }
+}
+
+
+export const Auth = (req:Request ,res:Response ,next:NextFunction)=>{
+     
+    if(!req.headers["authorization"]){
+        throw new BadAuthError("Authorization failed" , 401); 
+    }
+
+    const  [_ ,  token] =  req.headers["authorization"].split(" "); 
+   
+    if(!token){
+        throw new BadAuthError("Authorization failed", 401); 
+    }
+
+ 
+    try { 
+    
+        const payload =  jwt.verify(token , process.env.JWT_SECRET!)  as Payload ;
+             
+        req.user.id =  payload.id ; 
+      
+        
+    } catch (error) {
+        
+        console.log(error); 
+         throw new BadAuthError("Authorization failed", 401); 
+    }
+
+    next() ;
+
+}
