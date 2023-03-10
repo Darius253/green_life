8eff72fe3fcf84cac19c5832437e01b32be268e9
kelvin <denlinato@gmail.com>
2023-03-10 22:09:35 +0000

    changing login logic

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -4,7 +4,7 @@ import { BadAuthError } from '@utils/BadAuthError';
 import  {compare} from 'bcrypt' ;
 import  jwt from 'jsonwebtoken' ; 
   
-/* @ login controller for login in user returns 
+/* @ web app login controller for login in user returns 
 returns user details after checking if user exists.
 */
 
@@ -31,11 +31,12 @@ export async function  login(req:Request , res:Response) {
     
        //creat two jwt one as a refreshToken  and one as an accessToken
   //create a jwt for user   
-   const accessToken =  jwt.sign({id:user._id, email:user.email} , process.env.JWT_SECRET! , {expiresIn:"1hr"}) ; 
+   const accessToken =  jwt.sign({id:user._id, email:user.email} , process.env.JWT_SECRET! , {expiresIn:"15m"}) ; 
  const refreshToken = jwt.sign({id:user._id , email:user.email} , process.env.JWT_refresh! , {expiresIn:"1hr"}) ;
   //if its from the web app set an http only cookie  
+      
 
-   if(req.headers["user-agent"]=== "Mozilla"){
+            //create an http only cookie containing the refreshToken
     res.cookie("refreshToken" , refreshToken , {httpOnly:true , signed:true}) ; 
       return res.status(200).send({
        success: true,
@@ -44,17 +45,9 @@ export async function  login(req:Request , res:Response) {
          accessToken
        }
      });
-   }
+   
 
-  //if user is from the mobile app send refreshToken , including accessToken
- 
-    return  res.status(200).send({
-       success: true,
-       data: {
-         user,
-         accessToken, refreshToken
-       },
-     });
+
 
 }
 
@@ -78,6 +71,8 @@ export async function signup(req:Request , res:Response){
     if(phoneNumberExist){
         throw new BadAuthError("phone number already exist" , 400) ; 
     }
+
+
  //create and save user
      const user =  new User({
         name , email , phoneNumber  , password
@@ -93,6 +88,54 @@ export async function signup(req:Request , res:Response){
         success:true 
     })  
 
+
+}
+
+ //@ mobileLogin for users  with mobile apps ,create a refreshToken and accessToken
+ //sends both to user .
+export const mobileLogin =async (req:Request , res:Response) => {
+    
+     const { email, password } = req.body;
+
+     //check if user exist
+     const user = await User.findOne({ email });
+
+     if (!user) {
+       throw new BadAuthError("Email or password is incorrect", 401);
+     }
+
+     //compare password  , with user's hashpassword.. send error if password is incorrect
+
+     const isValid = await compare(password, user.password);
+
+     if (!isValid) {
+       throw new BadAuthError("Email or password is incorrect", 401);
+     }
+
+     //creat two jwt one as a refreshToken  and one as an accessToken
+     //create a jwt for user
+     const accessToken = jwt.sign(
+       { id: user._id, email: user.email },
+       process.env.JWT_SECRET!,
+       { expiresIn: "15m" }
+     );
+     const refreshToken = jwt.sign(
+       { id: user._id, email: user.email },
+       process.env.JWT_refresh!,
+       { expiresIn: "1hr" }
+     );
+     //if its from the web app set an http only cookie
+
+    //send the user , accessToken and refreshToken to the user
+     return res.status(200).send({
+       success: true,
+       data: {
+         user,
+         accessToken,
+         refreshToken
+       },
+     });
+   
 
 }
 
