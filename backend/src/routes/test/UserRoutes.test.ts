feee1c9b8f12dcb42c506a91bbfe819d85f0670f
Kelvin B <denlinato@gmail.com>
2023-03-12 17:42:20 +0000

    writing integration test

--- backend/src/routes/test/UserRoutes.test.ts
+++ backend/src/routes/test/UserRoutes.test.ts
@@ -136,7 +136,320 @@ test("expect to return a 400" , async ()=>{
 
   , 
 
-  test("it should return a 200 " , async ()=>{
+  test("it should return a 200 , should check if user is defined " , async ()=>{
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/login")
+    .send({
+        password:"223sdccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(200)
+    // console.log(logres.header)
+    expect(logres.body.data.user).toBeDefined()
 
   })
+
+
+  ,
+  test("should return  401 " , async ()=>{
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/login")
+    .send({
+        password:"223sccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(401)
+    // console.log(logres.header)
+    // expect(logres.body.data.user).toBeDefined()
+
+  }) , 
+
+  test("login , it should return a 200", async ()=>{
+        
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/mobileLogin")
+    .send({
+        password:"223sdccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(200)
+    // console.log(logres.header)
+    // expect(logres.headers["set-cookie"]).toBeDefined()
+
+ 
+
+
+
+
+ }) 
+
+
+ ,
+ test("login , it should return a 200 and body should be defined" ,  async ()=>{
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/mobileLogin")
+    .send({
+        password:"223sdccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(200)
+    // console.log(logres.header)
+    expect(logres.body.data.accessToken).toBeDefined()
+
+
+ })
+
+  , 
+
+  test("it should return a 200 , should check if user is defined " , async ()=>{
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/mobileLogin")
+    .send({
+        password:"223sdccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(200)
+    // console.log(logres.header)
+    expect(logres.body.data.user).toBeDefined()
+
+  })
+,
+test("it should return a 200 , should check if user is defined " , async ()=>{
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/mobileLogin")
+    .send({
+        password:"223sdccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(200)
+    // console.log(logres.header)
+    expect(logres.body.data.refreshToken).toBeDefined()
+
+  })
+
+
+  ,
+  test("should return  401 " , async ()=>{
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/mobileLogin")
+    .send({
+        password:"223sccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(401)
+    // console.log(logres.header)
+    // expect(logres.body.data.user).toBeDefined()
+
+  }) 
+
+   ,
+
+
+   test("should return a 200" ,async ()=>{
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/login")
+    .send({
+        password:"223sdccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(200)
+    // console.log(logres.header)
+    const  refreshToken=  logres.headers["set-cookie"][0]
+    // expect(logres.body.data.user).toBeDefined()
+      
+     
+   await request(app)
+    .get("/api/auth/request")
+    .set("Cookie" ,refreshToken )
+    .expect(200)
+  }) 
+    ,
+    test("should return a 401" ,async ()=>{
+        const res =  await request(app)
+        .post("/api/auth/signup")
+        .send({
+           name:"ekwekw" , 
+           phoneNumber:"121212121" , 
+           password:"223sdccerfefe"  ,
+           email:"lsdkslmer@gmial.com"
+        })
+        .expect(201) 
+    
+    
+       const logres =  await request(app)
+        .post("/api/auth/login")
+        .send({
+            password:"223sdccerfefe"  ,
+            email:"lsdkslmer@gmial.com"
+        })
+        .expect(200)
+        // console.log(logres.header)
+        const  refreshToken=  logres.headers["set-cookie"][0]
+        // expect(logres.body.data.user).toBeDefined()
+          
+         
+       await request(app)
+        .get("/api/auth/request")
+        
+        .expect(401)
+      }) 
+       
+   ,
+
+   test("should return a 200" ,async ()=>{
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/login")
+    .send({
+        password:"223sdccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(200)
+    // console.log(logres.header)
+    const  refreshToken=  logres.headers["set-cookie"][0]
+    // expect(logres.body.data.user).toBeDefined()
+      
+     
+  const reqRes=  await request(app)
+    .get("/api/auth/request")
+    .set("Cookie" ,refreshToken )
+    .expect(200)
+ 
+
+     expect(reqRes.body.data.accessToken).toBeDefined()
+    
+  }) 
+ ,
+ test("should return a 200" ,async ()=>{
+    const res =  await request(app)
+    .post("/api/auth/signup")
+    .send({
+       name:"ekwekw" , 
+       phoneNumber:"121212121" , 
+       password:"223sdccerfefe"  ,
+       email:"lsdkslmer@gmial.com"
+    })
+    .expect(201) 
+
+
+   const logres =  await request(app)
+    .post("/api/auth/mobileLogin")
+    .send({
+        password:"223sdccerfefe"  ,
+        email:"lsdkslmer@gmial.com"
+    })
+    .expect(200)
+    // console.log(logres.header)
+    const  refreshToken=  logres.headers["set-cookie"][0]
+    // expect(logres.body.data.user).toBeDefined()
+      
+     
+   await request(app)
+    .get("/api/auth/requestMobile")
+    .set("Cookie" ,refreshToken )
+    .expect(200)
+  }) 
+   
+
+
+
+   
 })
