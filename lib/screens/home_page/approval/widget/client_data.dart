89defd3a446d62abd9ffb688451bb6f764bd7101
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-19 13:11:47 +0000

    Made few changes

--- lib/screens/home_page/approval/widget/client_data.dart
+++ lib/screens/home_page/approval/widget/client_data.dart
@@ -9,7 +9,7 @@ class ClientData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
+    // final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
     return Row(
       children: [
